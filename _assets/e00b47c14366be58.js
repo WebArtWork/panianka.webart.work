a1d725a40868aab02642d902d92c7396364bd6c9
HOPEtv <dome.hope.tv@gmail.com>
2025-07-17 15:23:10 +0300

    Add files via upload

---
+++ _assets/e00b47c14366be58.js
@@ -0,0 +1,8 @@
+(self["webpackChunk_canva_web"] = self["webpackChunk_canva_web"] || []).push([[46495],{
+
+/***/ 891548:
+function(_, __, __webpack_require__) {__webpack_require__.n_x = __webpack_require__.n;const __web_req__ = __webpack_require__;__web_req__(905716);self._5f74ec40302898c5a55451c9fbd04240 = self._5f74ec40302898c5a55451c9fbd04240 || {};(function(__c) {__c.TXb=function(a){const b={...a.props};a.G.zr(({Kh:c,UH:d})=>{c=a.nGb?{}:{qd:c()};a.ga.track(a.event,{...c,om:d(),...b})})};__c.oH.prototype.FT=__c.ia(18,function(){});__c.yJ.prototype.FT=__c.ia(17,function(){this.ended||this.zL||(this.zL=!0,setTimeout(()=>{try{this.ended=!0,__c.q4a(this)}catch(a){this.I.Bb(a,{se:"Error ending span in next cycle",extra:new Map(__c.qJ(this))})}}))});
+}).call(self, self._5f74ec40302898c5a55451c9fbd04240);}
+
+}])
+//# sourceMappingURL=sourcemaps/e00b47c14366be58.js.map
